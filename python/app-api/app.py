b1333d3efab877670a354a6ff3623a2f4e1755bd
mahen035 <mahen518@gmail.com>
2024-10-30 19:30:12 +0530

    post api

--- python/app-api/app.py
+++ python/app-api/app.py
@@ -25,15 +25,45 @@ def get_data():
 
 @app.route('/data', methods=['POST'])
 def post_data():
-    cur = mysql.connection.cursor()
+    #cur = mysql.connection.cursor()
+    connection = create_connection()
+    cur = connection.cursor()
     id = request.json['id']
     name = request.json['emp_name']
     dept = request.json['emp_dept']
     salary = request.json['emp_sal']
     cur.execute('''INSERT INTO employee (emp_id, emp_name, emp_dept, emp_salary) VALUES(%s, %s, %s, %s)''', (id, name, dept, salary))
-    mysql.connection.commit()
+    #mysql.connection.commit()
     cur.close()
     return jsonify({'message':'Data inserted successfully'})
+
+# POST /api/submit
+# Accepts a JSON payload containing the quiz ID and answers,
+# then calculates the score. The request body should look like this:
+# {
+#     "quiz_id": 1,
+#     "answers": [
+#         {"question_id": 1, "choice_id": 2},
+#         {"question_id": 2, "choice_id": 5}
+#     ]
+# } 
+@app.route('/api/submit', methods=['POST'])
+def post_data():
+    #cur = mysql.connection.cursor()
+    connection = create_connection()
+    cur = connection.cursor()
+    answers = request.json['answers']
+    score = 0
+    #Check whether answer is correct or not
+    for answer in answers:
+        question_id = answer['question_id']
+        choice_id = answer['choice_id']
+        cur.execute("SELECT is_correct FROM choices where id = %s and question_id = %s",(choice_id, question_id))
+        result = cur.fetchone()
+        if result:
+            score += 1
+    return jsonify({"score":score})         
+    
 
 @app.route('/data/<int:id>', methods=['GET'])
 def get_data_by_id(id):
